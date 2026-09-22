fd3e54f1f70350985adb45c07086da911cbebbcb
Sharon Jessy T S <sharonthatigiri@gmail.com>
2026-09-22 08:51:50 +0530

    Objets

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
 // creating objects using literals -- {}
+//object --> it is a data structure that stores a related data as a key - value pair
 
 let details = {
     name : "Sharon",
@@ -35,3 +36,49 @@ delete details.designation;
 console.log(details);
 
 // for checking properties use 'in' keyword
+
+
+//Itrating over a object 
+//using for...in loop --> we use hasOwnProperty() - to ensure object as own properties 
+
+function iterateObject() {
+    let example = {
+        name : "sharon",
+        age : 22,
+        clg : "CIT"
+    };
+
+    for(let key in example){
+        if(Object.prototype.hasOwnProperty.call(example, key)){
+            console.log(example[key]);
+        }
+    }
+}
+iterateObject();
+
+function iterateObjectUsingEntries(){
+    let example = {
+        name : "sharon",
+        age : 22,
+        clg : "CIT"
+    };
+
+    Object.entries(example).forEach(([key,valuesss])=>{
+        console.log(key,valuesss);
+    });
+}
+iterateObjectUsingEntries();
+
+function iterateObjectUsingKeys(){
+     let example = {
+        name : "sharon",
+        age : 22,
+        clg : "CIT"
+    };
+
+    Object.keys(example).forEach(key => {
+        const value = example[key];
+        console.log(`${key}, ${value}`);
+    });
+
+}
